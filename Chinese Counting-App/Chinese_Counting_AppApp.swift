5a68d155a8d2851df0be3b7d8071e210f2bb2cd9
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-24 09:01:54 +0800

    - initialized project

---
+++ Chinese Counting-App/Chinese_Counting_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chinese_Counting_AppApp.swift
+//  Chinese Counting-App
+//
+//  Created by Ronjie Diafante Man-on on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Chinese_Counting_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
