2dcf2a3d857565df1b8a37835c4b6f005d79ec6c
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-25 15:54:33 +0800

    - added SplashScreenView

--- Chinese Counting/View/SplashScreenView.swift
+++ Chinese Counting/View/SplashScreenView.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct SplashScreenView: View {
+    @Binding var isPresented: Bool
+    
+    @State private var opacity = 0.0
+    @State private var scale = CGSize(width: 0.5, height: 0.5)
+    @State private var moveUp = false
+    @State private var angle = 45.0
+    @State private var spacing = 5.0
+    
+    var proverbVM = ProverbViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                Color
+                    .black
+                    .ignoresSafeArea()
+                Image("china1")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                    .frame(
+                        width: geometry.size.width,
+                        height: geometry.size.height
+                    )
+                    .opacity(opacity)
+                
+                VStack {
+                    VStack(spacing: spacing) {
+                        Text("Line 1")
+                            .foregroundStyle(.red)
+                        Text("Line 2")
+                            .foregroundStyle(.blue)
+                        Text("Line 3")
+                            .foregroundStyle(.green)
+                        
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 25)
+                        .fill(
+                            LinearGradient(
+                                colors: [.black, .gray],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                            .opacity(opacity)
+                    )
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .scaleEffect(scale)
+                    .rotation3DEffect(
+                        Angle(degrees: angle),
+                        axis: (x: 1.0, y: 0.0, z: 0.0))
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    SplashScreenView()
+    SplashScreenView(isPresented: .constant(true))
 }
