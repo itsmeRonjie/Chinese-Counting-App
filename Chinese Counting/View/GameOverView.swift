c18a47469df64b27bface4d5110c44a8a12c18c1
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-25 14:46:36 +0800

    - added GameOverView

--- Chinese Counting/View/GameOverView.swift
+++ Chinese Counting/View/GameOverView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct GameOverView: View {
+    @Environment(GameViewModel.self) var gameVM
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(
+                [
+                    .china1, .china2,
+                    .china3, .china4,
+                    .china5, .china6,
+                    .china7
+                ].randomElement() ?? .china1
+            )
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea()
+            .opacity(0.9)
+            
+            VStack {
+                VStack {
+                    Text("Game Over")
+                    Text(
+                        "You solved \(gameVM.gameModel.score) out of \(gameVM.gameModel.maxTurns)!"
+                    )
+                    Text("Tap anywhere to start again!")
+                        .font(.title)
+                        .fontWeight(.regular)
+                        .shadow(radius: 5)
+                }
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.foreground)
+                .padding()
+                .background(Material.ultraThin, in: RoundedRectangle(cornerRadius: 10))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.foreground, lineWidth: 1)
+                }
+
+                Spacer()
+            }
+            .padding()
+        }
+        .onTapGesture {
+            gameVM.gameModel.resetGame()
+        }
     }
 }
 
 #Preview {
     GameOverView()
+    .environment(GameViewModel())
 }
