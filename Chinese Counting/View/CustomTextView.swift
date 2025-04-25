57dd4a96bad089f2d189a7739847d679e9321e15
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-25 16:04:30 +0800

    - added CustomTextView

--- Chinese Counting/View/CustomTextView.swift
+++ Chinese Counting/View/CustomTextView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct CustomTextView: View {
+    let spacing: Double
+    let angle: Double
+    let scale: CGSize
+    let opacity: Double
+    let text1: String
+    let text2: String
+    let text3: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack(spacing: spacing) {
+            Text(text1)
+                .foregroundStyle(.red)
+            Text(text2)
+                .foregroundStyle(.blue)
+            Text(text3)
+                .foregroundStyle(.green)
+            
+        }
+        .padding()
+        .background(RoundedRectangle(cornerRadius: 25)
+            .fill(
+                LinearGradient(
+                    colors: [.black, .gray],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing
+                )
+            )
+                .opacity(opacity)
+        )
+        .font(.title)
+        .fontWeight(.semibold)
+        .multilineTextAlignment(.center)
+        .padding()
+        .scaleEffect(scale)
+        .rotation3DEffect(
+            Angle(degrees: angle),
+            axis: (x: 1.0, y: 0.0, z: 0.0))    }
 }
 
 #Preview {
-    CustomTextView()
+    CustomTextView(
+        spacing: 5,
+        angle: 45,
+        scale: CGSize(width: 0.5, height: 0.5),
+        opacity: 1,
+        text1: "Line 1",
+        text2: "Line 2",
+        text3: "Line 3"
+    )
 }
