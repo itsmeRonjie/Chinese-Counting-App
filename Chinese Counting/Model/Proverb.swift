ba2dd16a4c90369da69758241e7b99cbe60d6ac3
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-25 15:01:07 +0800

    - added ProverbModel

---
+++ Chinese Counting/Model/Proverb.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  Chinese Counting
+//
+//  Created by Ronjie Diafante Man-on on 4/25/25.
+//
+
+import Foundation
+
+// https://chinese-proverbs.onrender.com/api/proverbs/random
+
+
+// MARK: - ProverbModel
+struct ProverbModel: Codable {
+    let id: String
+    let proverb: String
+    let pinyin: String
+    let translation: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case proverb
+        case pinyin
+        case translation
+    }
+    
+    static func defaultProverb() -> ProverbModel {
+        ProverbModel(
+            id: "",
+            proverb: "留得青山在，不怕没柴烧。",
+            pinyin: "Liú dé qīng shān zài, bú pà méi chái shāo",
+            translation: "While there are green hills, there'll be wood to burn."
+        )
+    }
+}
